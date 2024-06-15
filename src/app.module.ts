01c25ec22050a908341d841dceb4161913fad95b
Dimi <tomuleseidimitrie@gmail.com>
2024-06-15 09:47:34 +0300

    add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { Task } from './tasks/task.entity';
 import { TaskRepository } from './tasks/task.repository';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
